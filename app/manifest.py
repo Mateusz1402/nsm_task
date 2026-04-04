caf5bb6b69ef77348c8b7140a28169f8b5a9da4d
Mateusz Sabat <mateusz.sabat38@gmail.com>
2026-04-04 16:56:40 +0200

    feat: verify_manifest() & reorganisation of main (logic goes to generate_manifest()) & testing new feature.

--- app/manifest.py
+++ app/manifest.py
@@ -22,27 +22,56 @@ def get_hash(file_path):
         print(f"Generating a SHA256 hash from the file {file_path} occurs an error: {e}")
         sys.exit(1)
 
+def verify_manifest(input_dir, manifest_path):
+    #Verifying whole manifest list for SHA256 hash mismatches.
+    print(f"Veryfing directory against manifest: {manifest_path}")
+    mismatches = 0
+    try:
+        with open(manifest_path, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue
+                #Spaces case (directory in the " ").
+                if line.startswith('"'):
+                    #Seraching for second char of " (end of dir).
+                    parts = line[1:].split('"', 1)
+                    #Left side.
+                    rel_path = parts[0]
+                    #Right side.
+                    expected_hash = parts[1].strip()
+                #No spaces case.
+                else:
+                    parts = line.split(" ", 1)
+                    rel_path = parts[0]
+                    expected_hash = parts[1].strip()
+                
+                full_path = os.path.join(input_dir, rel_path)
 
+                #Check whether file exists.
+                if not os.path.exists(full_path):
+                    print(f"Missing the file: {rel_path}")
+                    mismatches += 1
+                    continue
 
-
-
-def main():
-    parser = argparse.ArgumentParser(description="NMS directory manifest tool")
-    #Input:
-    parser.add_argument("-input_dir", required=True, help="Directory to scan")
-    #Output:
-    parser.add_argument("-output", required=True, help="Output manifest file")
-
-    args = parser.parse_args()
-    #Take absolute input directory (if symlink)
-    input_dir = os.path.abspath(args.input_dir)
-    #Secure input from hazardous
-    if not os.path.isdir(input_dir):
-        print(f"Error: {input_dir} is not a directory!")
+                current_hash = get_hash(full_path)
+                if current_hash == expected_hash:
+                    print(f"OK: {rel_path}")
+                else:
+                    print(f"Failed: {rel_path} (Hash mismatch)")
+                    mismatches += 1
+        if mismatches == 0:
+            print("\nVerification successful! All files match.")
+        else:
+            print(f"\nVerification failed! Found {mismatches} mismatches.")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Error during verification: {e}")
         sys.exit(1)
+                
 
-    output_file = args.output
-
+def generate_manifest(input_dir):
+    #Generating new manifest list.
     manifest_list = []
 
 
@@ -78,13 +107,42 @@ def main():
                 formatted_path = rel_path
 
             manifest_list.append(f"{formatted_path} {checksum}")
-    try:
-        with open(output_file, "w", encoding="utf-8") as file:
-            file.write("\n".join(manifest_list) + "\n")
-        print(f"Manifest file successfully saved to {output_file}")
-    except Exception as e:
-        print(f"Error while writing manifest: {e}")
+    return manifest_list
+
+
+def main():
+    parser = argparse.ArgumentParser(description="NMS directory manifest tool")
+    #Input:
+    parser.add_argument("-input_dir", required=True, help="Directory to scan")
+    #Output:
+    parser.add_argument("-output", required=True, help="Output manifest file")
+    # BONUS part:
+    parser.add_argument("--check", action="store_true", help="Enable verification mode")
+
+
+    args = parser.parse_args()
+    #Take absolute input directory (if symlink)
+    input_dir = os.path.abspath(args.input_dir)
+    #Secure input from hazardous
+    if not os.path.isdir(input_dir):
+        print(f"Error: {input_dir} is not a directory!")
         sys.exit(1)
+
+    output_file = args.output
+
+    #Whether verify function to use.
+    if args.check:
+        verify_manifest(input_dir, output_file)
+    else:
+        manifest_list = generate_manifest(input_dir)
+        try:
+            with open(output_file, "w", encoding="utf-8") as file:
+                file.write("\n".join(manifest_list) + "\n")
+            print(f"Manifest file successfully saved to {output_file}")
+        except Exception as e:
+            print(f"Error while writing manifest: {e}")
+            sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
